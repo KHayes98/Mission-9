400f8756ad84615b6ba96cd81b7413841fec3b49
Reboot <kchayes2016@gmail.com>
2023-03-06 20:02:09 -0700

    Update

---
+++ Mission_9/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission_9.Models;
+using Mission_9.Models.View_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+        
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+    }
+}
---
+++ Mission_9/Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_9.Models.View_Models
+{
+    public class BooksViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
